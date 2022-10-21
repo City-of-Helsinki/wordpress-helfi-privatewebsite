dd32632ee2defd212e74477bccf251bf18fc6b43
GarfTornion <billy@broomudigitals.fi>
2022-10-21 10:03:24 +0300

    feature/WY-277 extend .htaccess to forward to a php file to perform login check, merge media file protection setting with login enabled setting

--- media-access/manage.php
+++ media-access/manage.php
@@ -5,9 +5,6 @@ namespace CityOfHelsinki\WordPress\PrivateWebsite;
 add_action('helsinki_privatewebsite_init', __NAMESPACE__ . '\\privatewebsite_check_media_restriction_file');
 
 function privatewebsite_upload_dir_path() {
-    if (is_multisite()) {
-        return wp_upload_dir()['basedir'].'/sites/'.get_current_blog_id();
-    }
     return wp_upload_dir()['basedir'];
 }
 
@@ -17,31 +14,34 @@ function privatewebsite_check_media_restriction_file() {
 
     $restriction_enabled = '';
     $settings = get_option('helsinki-privatewebsite-settings', array());
-    if (isset($settings['protect-media-files'])) {
-        $restriction_enabled = $settings['protect-media-files'];
+    if (isset($settings['login-page-enabled'])) {
+        $restriction_enabled = $settings['login-page-enabled'];
     }
 
     if (isset($restriction_enabled) && $restriction_enabled === 'on') {
-        if (!file_exists($path . '.htaccess')) {
-            privatewebsite_create_media_restriction_file();
+        if (!file_exists($path . '.htaccess') || !file_exists($path . 'dl-file.php')) {
+            privatewebsite_create_media_restriction_files();
         }
     }
     else {
-        if (file_exists($path . '.htaccess')) {
-            privatewebsite_remove_media_restriction_file();
+        if (file_exists($path . '.htaccess') || file_exists($path . 'dl-file.php')) {
+            privatewebsite_remove_media_restriction_files();
         }
     }
 }
 
-function privatewebsite_create_media_restriction_file() {
+function privatewebsite_create_media_restriction_files() {
     $path = trailingslashit( privatewebsite_upload_dir_path() );
-    $file = PLUGIN_PATH . 'media-access/.htaccess';
-    copy($file, $path . '.htaccess' );
+    $htAccessfile = PLUGIN_PATH . 'media-access/.htaccess';
+    $dlFile = PLUGIN_PATH . 'media-access/dl-file.php';
+    copy($htAccessfile, $path . '.htaccess' );
+    copy($dlFile, $path . 'dl-file.php' );
 }
 
-function privatewebsite_remove_media_restriction_file() {
+function privatewebsite_remove_media_restriction_files() {
     $path = trailingslashit( privatewebsite_upload_dir_path() );
     if (file_exists($path . '.htaccess')) {
         unlink($path . '.htaccess' );
+        unlink($path . 'dl-file.php' );
     }
 }
