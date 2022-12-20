d66146dea8504de068c51c3bda6f8b8a922c87e8
GarfTornion <billy@broomudigitals.fi>
2022-12-20 09:06:05 +0200

    version bump

--- wordpress-helfi-privatewebsite.php
+++ wordpress-helfi-privatewebsite.php
@@ -3,7 +3,7 @@
 /**
   * Plugin Name: Helsinki Private Website
   * Description: Hides the website content behind a login.
-  * Version: 1.0.0
+  * Version: 1.0.1
   * License: MIT
   * Requires at least: 5.7
   * Requires PHP:      7.1
@@ -18,7 +18,7 @@ namespace CityOfHelsinki\WordPress\PrivateWebsite;
 /**
  * Constants
 */
-define( __NAMESPACE__ . '\\PLUGIN_VERSION', '1.0.0' );
+define( __NAMESPACE__ . '\\PLUGIN_VERSION', '1.0.1' );
 define( __NAMESPACE__ . '\\PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( __NAMESPACE__ . '\\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( __NAMESPACE__ . '\\PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
