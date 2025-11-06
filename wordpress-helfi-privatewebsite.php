be85ab9fa01e6bcc23afe5470f450bacd3b277c3
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-11-06 12:37:51 +0200

    Update wordpress-helfi-privatewebsite.php

--- wordpress-helfi-privatewebsite.php
+++ wordpress-helfi-privatewebsite.php
@@ -3,10 +3,10 @@
 /**
   * Plugin Name: Helsinki Private Website
   * Description: Hides the website content behind a login.
-  * Version: 1.8.1
+  * Version: 1.9.0
   * License: GPLv3
   * Requires at least: 5.7
-  * Requires PHP:      7.1
+  * Requires PHP: 8.2
   * Author: Broomu Digitals
   * Author URI: https://www.broomudigitals.fi
   * Text Domain: helsinki-privatewebsite
